7e26027559af03d8742736dc937f3bbf8239a4fa
YoakamWang <90314896+YoakamWang@users.noreply.github.com>
2022-09-16 17:26:16 +0800

    Add files via upload

--- py-pdf.py
+++ py-pdf.py
@@ -1,34 +1,50 @@
+import time
+
 import pandas as pd
 import pdfplumber
 import shutil
 from pathlib import Path
+import os
+import sys
 
 tags = []
 
 
-def extract_table_info(filepath):
-    writer = pd.ExcelWriter('test.xlsx', engine="openpyxl")
-    with pdfplumber.open(filepath) as pdf:
-        page = pdf.pages[0]
-        # table_info = page.extract_table()
-        # df_table = pd.DataFrame(table_info[1:], columns=table_info[0])
-        # df_table.to_csv('dmeo.csv', index=False, encoding='gbk')
+def get_path():
+    if len(sys.argv) < 2:
+        print("Please try again")
+        sys.exit()
+    path = sys.argv[1]
+    rel_path = os.path.split(path)
+    basepath = rel_path[0]
+    filename = rel_path[1]
+    exe_name = os.path.splitext(filename)
+    rel_filename = exe_name[0]
+    return basepath, rel_filename
 
-        tables_info = page.extract_tables()
-        for index in range(len(tables_info)):
-            # 设置表格的第一行为表头，其余为数据
-            df_table = pd.DataFrame(tables_info[index][1:], columns=tables_info[index][0])
-            # print(df_table.iloc[0][3])
-            # df_table=df_table.append(df_table,ignore_index=True) #There are many warning for now so have to use concat
-            # df_table = pd.concat([df_table, df_table], ignore_index=True)
-            # print(df_table)
-            df_table.to_excel("dmeo" + str(index) + ".xlsx", index=False)
-            tags.append("dmeo" + str(index) + ".xlsx")
-        print(tags)
-        for tag_value in tags:
-            data = pd.read_excel(tag_value, engine="openpyxl")
-            data.to_excel(writer, tag_value, index=False)
-    writer.save()
+
+def extract_table_info(filepath):
+    global tags
+    with pdfplumber.open(filepath) as pdf:
+        for i in range(len(pdf.pages)):
+            page = pdf.pages[i]
+            print(i)
+            writer = pd.ExcelWriter('test' + str(i) + ".xlsx", engine="openpyxl")
+            tables_info = page.extract_tables()
+            for index in range(len(tables_info)):
+                # 设置表格的第一行为表头，其余为数据
+                df_table = pd.DataFrame(tables_info[index][1:], columns=tables_info[index][0])
+                df_table.to_excel("dmeo" + str(index) + ".xlsx", index=False)
+                tags.append("dmeo" + str(index) + ".xlsx")
+            print(tags)
+            for tag_value in tags:
+                data = pd.read_excel(tag_value, engine="openpyxl")
+                data.to_excel(writer, tag_value, index=False)
+            writer.save()
+            writer.close()
+            del_file()
+            tags = []
+            # time.sleep(5)
 
 
 def del_file():
@@ -38,47 +54,4 @@ def del_file():
 
 
 if __name__ == "__main__":
-    extract_table_info('56511e00_drw.pdf')
-    del_file()
-
-# import pandas as pd
-# import tabula
-# tabula.convert_into("56112E01_drw.pdf","out.csv",output_format="csv",pages=1)
-# table_info.to_csv('dmeo.csv', index=False, encoding='gbk')
-# import camelot
-# tabals=camelot.read_pdf("56511e00_drw.pdf")
-# tabals.export('foo.csv', f='csv', compress=True)
-# import tabula
-# df=tabula.read_pdf("56112E01_drw.pdf", area=(2200,1220,2800,2462), pages=1)
-# for index in range(len(df)):
-#     df_table = pd.DataFrame(df[index][1:], columns=df[index])
-# print(df_table.head())
-# import pdfplumber
-# from PyPDF2 import PdfFileReader
-#
-# pdf_reader=PdfFileReader("56511e00_drw.pdf")
-# with pdfplumber.open("56511e00_drw.pdf") as pdf:
-#     for i in range(pdf_reader.getNumPages()):
-#         page=pdf.pages[i]
-#         table=page.extract_tables(table_settings={
-# 			"vertical_strategy":"text",
-# 			"horizontal_strategy":"text",
-# 		})
-
-# import pdfplumber
-# from openpyxl import Workbook
-# with pdfplumber.open("56511e00_drw.pdf") as pdf:
-# 	table_page1 = pdf.pages[0]
-# 	table = table_page1.extract_table(
-# 		table_settings={
-# 			"vertical_strategy":"text",
-# 			"horizontal_strategy":"text",
-# 		})
-# 	print(table)
-#
-# workbook = Workbook()
-# sheet = workbook.active
-# for row in table:
-# 	if not ''.join([str(item) for item in row]) == '':
-# 		sheet.append(row)
-# 	workbook.save(filename="4.xlsx")
+    extract_table_info("56511e00_drw.pdf")
