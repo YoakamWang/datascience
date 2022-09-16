0dbc79194860278d6f986b253d2020ceaa7db721
YoakamWang <90314896+YoakamWang@users.noreply.github.com>
2022-09-16 13:49:40 +0800

    Add files via upload

--- py-pdf.py
+++ py-pdf.py
@@ -1,24 +1,46 @@
 import pandas as pd
 import pdfplumber
+import shutil
+from pathlib import Path
+
+tags = []
 
 
 def extract_table_info(filepath):
+    writer = pd.ExcelWriter('test.xlsx', engine="openpyxl")
     with pdfplumber.open(filepath) as pdf:
-        page = pdf.pages[1]
+        page = pdf.pages[0]
         # table_info = page.extract_table()
         # df_table = pd.DataFrame(table_info[1:], columns=table_info[0])
         # df_table.to_csv('dmeo.csv', index=False, encoding='gbk')
+
         tables_info = page.extract_tables()
         for index in range(len(tables_info)):
             # 设置表格的第一行为表头，其余为数据
             df_table = pd.DataFrame(tables_info[index][1:], columns=tables_info[index][0])
             # print(df_table.iloc[0][3])
-            # df_table=df_table.append(df_table,ignore_index=True) There are many warning for now so have to use concat
-            df_table = pd.concat([df_table, df_table], ignore_index=True)
-        df_table.to_csv("dmeo.csv", index=False, encoding='gbk')
+            # df_table=df_table.append(df_table,ignore_index=True) #There are many warning for now so have to use concat
+            # df_table = pd.concat([df_table, df_table], ignore_index=True)
+            # print(df_table)
+            df_table.to_excel("dmeo" + str(index) + ".xlsx", index=False)
+            tags.append("dmeo" + str(index) + ".xlsx")
+        print(tags)
+        for tag_value in tags:
+            data = pd.read_excel(tag_value, engine="openpyxl")
+            data.to_excel(writer, tag_value, index=False)
+    writer.save()
 
 
-extract_table_info('56511e00_drw.pdf')
+def del_file():
+    for elme in tags:
+        elm = Path(elme)
+        elm.unlink() if elm.is_file() else shutil.rmtree(elm)
+
+
+if __name__ == "__main__":
+    extract_table_info('56511e00_drw.pdf')
+    del_file()
+
 # import pandas as pd
 # import tabula
 # tabula.convert_into("56112E01_drw.pdf","out.csv",output_format="csv",pages=1)
